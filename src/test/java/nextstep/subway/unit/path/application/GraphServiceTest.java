f136385168636fef4bbb18fb5d50e538285a3548
Giwan Kim <giwan.kim@gmail.com>
2024-07-18 09:48:41 +0900

    feat(path): 지하철 그래프를 불러오는 서비스 클래스를 구현했다.

---
+++ src/test/java/nextstep/subway/unit/path/application/GraphServiceTest.java
@@ -0,0 +1,47 @@
+package nextstep.subway.unit.path.application;
+
+import static nextstep.subway.support.Fixtures.강남_역삼_구간;
+import static nextstep.subway.support.Fixtures.이호선;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+
+import java.util.List;
+import nextstep.subway.line.application.LineService;
+import nextstep.subway.line.domain.Line;
+import nextstep.subway.line.domain.LineSection;
+import nextstep.subway.path.application.GraphService;
+import nextstep.subway.path.domain.SubwayGraph;
+import nextstep.subway.station.domain.Station;
+import org.jgrapht.graph.DefaultWeightedEdge;
+import org.jgrapht.graph.WeightedMultigraph;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+@DisplayName("경로 그래프 서비스 단위 테스트")
+@SuppressWarnings("NonAsciiCharacters")
+class GraphServiceTest {
+  @Mock LineService lineService;
+  @InjectMocks GraphService graphService;
+
+  @DisplayName("노선 정보를 바탕으로 그래프를 불러온다.")
+  @Test
+  void loadGraph() {
+    Line line = 이호선();
+    LineSection section = 강남_역삼_구간();
+    given(lineService.findAllLines()).willReturn(List.of(line));
+
+    SubwayGraph graph = graphService.loadGraph();
+
+    SubwayGraph expectedGraph =
+        new SubwayGraph(
+            WeightedMultigraph.<Station, DefaultWeightedEdge>builder(DefaultWeightedEdge.class)
+                .addEdge(section.getUpStation(), section.getDownStation(), section.getDistance())
+                .build());
+    assertThat(graph.isSame(expectedGraph)).isTrue();
+  }
+}
